1e763963d3eaf9d85e45ce7461dcfdbe6ed87b78
jasonhargrove <jason@objas.com>
2016-07-26 12:31:34 -0400

    fix cors handling (for dev)

--- src/config/index.js
+++ src/config/index.js
@@ -2,23 +2,19 @@ const config = {
   env: process.env.NODE_ENV,
   name: 'skonkworks',
 
-  app: {
-
-  },
-
-  // The CORS support needs more attention
+  app: {},
 
   server: {
-    cors: {
-      origin: 'http://localhost:7777'
-    },
-
     port: 8888
   }
 };
 
 if (config.env === 'development') {
   config.foo = 'bar:development';
+
+  config.server.cors = {
+    origin: 'http://localhost:7777'
+  };
 
 } else if (config.env === 'production') {
   config.foo = 'bar:production';
