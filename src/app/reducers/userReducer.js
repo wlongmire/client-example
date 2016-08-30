5f47612bb195166d3bd69e23d515cb0c17b3a634
Alex Albino <webmaster@alexventure.com>
2016-08-29 22:50:28 -0700

    Issue #10 - clean up work for PR

--- src/app/reducers/userReducer.js
+++ src/app/reducers/userReducer.js
@@ -2,7 +2,7 @@ import * as constants from '../constants';
 
 const userReducer = (state = {}, {type, payload}) => {
     
-    if (payload && payload.state && payload.state.type == constants.USER_LOGGED_IN) {
+    if (payload && payload.state && payload.state.type === constants.USER_LOGGED_IN) {
         return payload;
     }
     if (payload && payload.state && payload.state.type === constants.USER_LOGGED_OUT) {
