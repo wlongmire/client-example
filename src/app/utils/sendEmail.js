817c7f4cd12379a7431fc51006e23c30cf8b6e1d
Warren Longmire <warrenlongmire@gmail.com>
2017-04-06 13:58:05 -0400

    adding testing code.

--- src/app/utils/sendEmail.js
+++ src/app/utils/sendEmail.js
@@ -4,8 +4,6 @@ import config from '../../config';
 let baseURL = config.apiserver.url;
 
 function sendEmail(emailAddress = "warrenlongmire@gmail.com", emailType="nonQuoteBroker", submissionId = "58e64ef6db7bda6f5eae60b0") {
-  console.log("in the service");
-
   return fetch(baseURL + `/api/sendEmail/${submissionId}`, {
     method: 'POST',
     headers: {
