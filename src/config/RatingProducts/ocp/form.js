dd11147e5a999f31293dcb4a6fb07a3b32d15980
Andriy Kulak <andriy.kulak@NYC-C02SV4HSH03Q.local>
2017-04-07 10:41:34 -0400

    added fix for truncating of OCP totalCost

--- src/config/RatingProducts/ocp/form.js
+++ src/config/RatingProducts/ocp/form.js
@@ -88,8 +88,7 @@ const ocpData = {
 	    "name": "totalCost",
       "required": true,
 	    "inputType": "text",
-	    "inputFormat": "currency",
-	    "tooltiptext": "Total Costs means the total cost of all work let or sublet including: a) the cost of all labor, materials and equipment furnished, used or delivered for use in the execution of the work and b) all fees bonuses or commissions made, paid or due."
+	    "inputFormat": "currency"
 	   },
      {
 	    "questionId": "5",
