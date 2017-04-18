ab442dd42fb7f2e7a2ea18a51cd2b2b2e06b4394
Andriy Kulak <andriy.kulak@NYC-C02SV4HSH03Q.T-mobile.com>
2017-04-17 21:58:19 -0400

    added minor config

--- src/config/index.js
+++ src/config/index.js
@@ -49,7 +49,7 @@ var config = {
     app: {},
 
     server: {
-        url: 'http://beta.ownersedge.us',
+        url: 'http://dev.ownersedge.us',
         port: null
     },
 
@@ -58,7 +58,7 @@ var config = {
     },
 
     apiserver: {
-        url: 'http://beta.ownersedge.us',
+        url: 'http://dev.ownersedge.us',
         port:  null
     }
 };
