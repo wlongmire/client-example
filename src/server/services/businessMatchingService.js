40809c77da65c5e15d54298306fb570f3bd8589c
Warren Longmire <warrenlongmire@gmail.com>
2017-04-20 17:22:17 -0400

    adding hercules Call.

--- src/server/services/businessMatchingService.js
+++ src/server/services/businessMatchingService.js
@@ -32,27 +32,54 @@ function getBusinessMatching(key, compare) {
 function getBusinessMatchingHercules(key, compare) {
   return new Promise((resolve, reject) => {
 
-    const inputs = compare.map((c)=>({
-      "compName":key.name, "compAdd":key.address,
-      "webName":c.name, "webAdd":c.address
-    }))
+    var params = {
+      "authToken": "1231213",
+      "functionality": "clearanceMatching",
+      "input": [{
+        "compName": "Argo Group right in Soho", 
+        "compAddress": "600 Broadway Street, 10019", 
+        "webName": "Argo Group on top of Soho | ArgoGroup on Mars | Argo Group right in Soho", 
+        "webAddress": "600 Broadway Street, 10019 | 600 Houston, NY | 600 Broadway Street, 10019"
+      }]
+    }
 
-    const matches = _.sortBy(inputs.map((m)=>({
-      name: m.webName,
-      compName:m.compName,
-      address: m.webAdd,
-      compAddress:m.compAdd,
-      nameProb: matcher(`${m.compName}`, `${m.webName}`),
-      addressProb: matcher(`${m.compAdd}`, `${m.webAdd}`)
-    })), ["nameProb", "addressProb"])
-    .reverse()
-    .filter((s)=>(s.nameProb > 0.9 && s.addressProb > 0.9))
-    .slice(0,3)
+    var options = {
+        method: 'POST',
+        uri: 'http://35.167.95.103:7070/SmartSearch/getHerculesData',
+        body: params,
+        headers: {
+          "Content-Type":"application/json"
+        }
+    };
 
-    resolve({
-      success:true,
-      matches
-    })
+    request(options).then((resp)=>{
+      resolve({
+        success:true,
+        matches:[]
+      })
+    });
+
+    // const inputs = compare.map((c)=>({
+    //   "compName":key.name, "compAdd":key.address,
+    //   "webName":c.name, "webAdd":c.address
+    // }))
+
+    // const matches = _.sortBy(inputs.map((m)=>({
+    //   name: m.webName,
+    //   compName:m.compName,
+    //   address: m.webAdd,
+    //   compAddress:m.compAdd,
+    //   nameProb: matcher(`${m.compName}`, `${m.webName}`),
+    //   addressProb: matcher(`${m.compAdd}`, `${m.webAdd}`)
+    // })), ["nameProb", "addressProb"])
+    // .reverse()
+    // .filter((s)=>(s.nameProb > 0.9 && s.addressProb > 0.9))
+    // .slice(0,3)
+
+    // resolve({
+    //   success:true,
+    //   matches
+    // })
   })
 }
 
