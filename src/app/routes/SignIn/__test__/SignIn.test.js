a4414d63fd5a962c6c0a7f48d3702e7a9d71788b
Warren Longmire <warrenlongmire@gmail.com>
2017-05-11 14:54:11 -0400

    adding first jest test

--- src/app/routes/SignIn/__test__/SignIn.test.js
+++ src/app/routes/SignIn/__test__/SignIn.test.js
@@ -1,6 +1,11 @@
-const sum = (a,b)=>{
-  return(a + b);
-};
+function sum(a, b){
+  return(a + b )
+}
 
+describe("<SignIn />", () => {
+  it('adds 1 + 2 to equal 3', ()=>{
+    expect(sum(1,2)).toBe(3)
+  })
+})
 
-
+console.log(window)
