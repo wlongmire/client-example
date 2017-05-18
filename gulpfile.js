38f22cb93915ae39a9ba6798101cfd8800dd2761
Warren Longmire <warrenlongmire@gmail.com>
2017-05-18 15:34:48 -0400

    adding rating engine url.

--- gulpfile.js
+++ gulpfile.js
@@ -29,7 +29,7 @@ gulp.task('transform:prod', function(){
   .pipe(replace('@serverUrl', 'http://www.ownersedge.us'))
   .pipe(replace('@appId', '57ab6abcf36d2840aa667f6e'))
   .pipe(replace('@mongoURI', 'mongodb://xxread:xxread@52.25.41.113:27017/ownersedgedev'))
-  .pipe(replace('@ratingsUrl', 'https://oe-rating-engine.herokuapp.com'))
+  .pipe(replace('@ratingsUrl', 'http://ownersedge-ratings-prod.us-east-1.elasticbeanstalk.com/'))
   .pipe(gulp.dest('src/config/'));
 });
 
@@ -41,7 +41,7 @@ gulp.task('transform:dev', function(){
   .pipe(replace('@serverUrl', 'http://dev.ownersedge.us'))
   .pipe(replace('@appId', '57ab6abcf36d2840aa667f6e'))
   .pipe(replace('@mongoURI', 'mongodb://apiuser:apipass@ds153765.mlab.com:53765/ownersedgedev'))
-  .pipe(replace('@ratingsUrl', 'https://oe-rating-engine-dev.herokuapp.com'))
+  .pipe(replace('@ratingsUrl', 'http://ownersedge-ratings-prod.us-east-1.elasticbeanstalk.com/'))
   .pipe(gulp.dest('src/config/'));
 });
 
@@ -53,7 +53,7 @@ gulp.task('transform:beta', function(){
   .pipe(replace('@serverUrl', 'http://beta.ownersedge.us'))
   .pipe(replace('@appId', '57ab6abcf36d2840aa667f6e'))
   .pipe(replace('@mongoURI', 'mongodb://apiuser:apipass@ds153765.mlab.com:53765/ownersedgedev'))
-  .pipe(replace('@ratingsUrl', 'https://oe-rating-engine-dev.herokuapp.com'))
+  .pipe(replace('@ratingsUrl', 'http://ownersedge-ratings-prod.us-east-1.elasticbeanstalk.com/'))
   .pipe(gulp.dest('src/config/'));
 });
 
@@ -65,7 +65,7 @@ gulp.task('transform:local', function(){
   .pipe(replace('@serverUrl', 'http://localhost'))
   .pipe(replace('@appId', '57ab6abcf36d2840aa667f6e'))
   .pipe(replace('@mongoURI', 'mongodb://apiuser:apipass@ds153765.mlab.com:53765/ownersedgedev'))
-  .pipe(replace('@ratingsUrl', 'https://oe-rating-engine-dev.herokuapp.com'))
+  .pipe(replace('@ratingsUrl', 'http://ownersedge-ratings-prod.us-east-1.elasticbeanstalk.com/'))
   .pipe(gulp.dest('src/config/'));
 });
 
