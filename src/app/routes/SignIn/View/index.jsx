0517336ba0cdb023ac59dab46d0468f29a200279
Warren Longmire <warrenlongmire@gmail.com>
2017-05-22 16:05:04 -0400

    adding signin through cognito.

--- src/app/routes/SignIn/View/index.jsx
+++ src/app/routes/SignIn/View/index.jsx
@@ -1,129 +1,98 @@
-import React, {Component} from 'react';
-import { push } from 'react-router-redux';
-import { connect } from 'react-redux';
+import React, { Component } from 'react'
+import { push } from 'react-router-redux'
+import { connect } from 'react-redux'
 
 import FormBuilder from 'components/shared/FormBuilder'
-import config from 'config';
+import config from 'config'
 import form from './form.js'
 
 import ToggleDisplay from 'components/shared/ToggleDisplay'
 
-import fetch from 'isomorphic-fetch';
-
-import mx from 'app/utils/MixpanelInterface';
+import mx from 'app/utils/MixpanelInterface'
 import { Button } from 'react-bootstrap'
 
+import { CognitoUser, CognitoUserPool, AuthenticationDetails } from 'amazon-cognito-identity-js'
+
 class SignInForm extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-          error:false,
-          errorMessage: ""
-      }
-
-      this.handleSubmit = this.handleSubmit.bind(this);
+  constructor(props) {
+    super(props)
+    this.state = {
+      error: false,
+      errorMessage: ''
     }
 
-    handleSubmit(values, controlGroups, requiredFields) {
-        const baseURL = config.apiserver.url;
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
 
-        if (values.username === "") {
-            this.setState({
-                error:true,
-                errorMessage:"Please Enter a Valid Username."
-            })
-        } else if (values.password === "") {
-            this.setState({
-                error:true,
-                errorMessage:"Please Enter a Valid Password."
-            })
-        } else {
-            
-            fetch(baseURL + '/um/login', {
-                method: 'POST',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(values)
-            })
-            .then(res => res.json())
-            .then((res) => {
-                switch(res.message) {
-                    case("Your account has not been verified. Please contact your administrator."):
-                        this.setState({
-                            error:true,
-                            errorMessage:"No registered account under that username."
-                        })
-                        return;
+  handleSubmit(values) {
+    if (values.username === '') {
+      this.setState({
+        error: true,
+        errorMessage: 'Please Enter a Valid Username.'
+      })
+    } else if (values.password === '') {
+      this.setState({
+        error: true,
+        errorMessage: 'Please Enter a Valid Password.'
+      })
+    } else {
+      const userPool = new CognitoUserPool({
+        UserPoolId: config.awsCognito.userPoolId,
+        ClientId: config.awsCognito.clientId
+      })
 
-                    case("Password or username are incorrect"):
-                        this.setState({
-                            error:true,
-                            errorMessage:"That Username / Password combination is incorrect"
-                        })
-                        return;
-                }
+      const cognitoUser = new CognitoUser({
+        Username: values.username,
+        Pool: userPool
+      })
 
-                const {user, token} = res;
+      const authenticationData = new AuthenticationDetails({
+        Username: values.username,
+        Password: values.password
+      })
 
-                localStorage.setItem('token', token);
-                localStorage.setItem('viewer', JSON.stringify(user));
-
-                this.setState({
-                    error:false,
-                    errorMessage:""
-                })
-                this.props.dispatch(push({
-                    pathname: '/submissions',
-
-                    state: {
-                        type: 'USER_LOGGED_IN',
-                        payload: res,
-                        user: user
-                    }
-                }));
-            })
-            .catch((e) => {
-                this.setState({
-                    error:true,
-                    errorMessage:"There appears to be an issue with our servers. Please contact us below for help."
-                })
-            });
-            
+      cognitoUser.authenticateUser(authenticationData, {
+        onSuccess: (resp) => {
+          console.log("login successful ",resp)
+        },
+        onFailure: (resp) => {
+          console.log("failed login ", resp)
+        },
+        newPasswordRequired: (resp) => {
+          console.log("new password required ", resp)
         }
-        
+      })
     }
+  }
 
-    render() {
-        const {
-            handleSubmit
-        } = this.props;
+  render() {
+    const {
+      handleSubmit
+    } = this.props
 
-        return (
-        <div className="SignInForm__container">
+    return (
+      <div className="SignInForm__container">
 
-            <h1>Welcome</h1>
-            <h3>Please Sign In</h3>
-            
-            <FormBuilder
-                data={form}
-                submitTitle="Sign In"
-                submissionButtons={()=>(
-                    <div>
-                        <ToggleDisplay 
-                            show={this.state.error}
-                            render={()=><div className="errorMessage">{ this.state.errorMessage }</div>}/>
-                        <Button bsStyle="primary" type="submit">Sign In</Button>
-                        {/*<Button bsStyle="primary" href="/signup">Register</Button>*/}
-                    </div>
-                )}
-                handleSubmit={this.handleSubmit}
-            />
+        <h1>Welcome</h1>
+        <h3>Please Sign In</h3>
 
-            
-        </div>)
-    }
+        <FormBuilder
+          data={form}
+          submitTitle="Sign In"
+          submissionButtons={() => (
+            <div>
+              <ToggleDisplay
+                show={this.state.error}
+                render={() => <div className="errorMessage">{ this.state.errorMessage }</div>}
+              />
+              <Button bsStyle="primary" type="submit">Sign In</Button>
+              {/*<Button bsStyle='primary' href='/signup'>Register</Button>*/}
+            </div>
+          )}
+          handleSubmit={this.handleSubmit}
+        />
+      </div>)
+  }
 }
 
 export default connect()(SignInForm)
--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
-'use strict';
+'use strict'
 
-var config = {
+const config = {
     env: process.env.NODE_ENV,
     name: 'ownersEdge',
     appId: '57ab6abcf36d2840aa667f6e',
@@ -22,11 +22,19 @@ var config = {
     ratingsUrl:'http://ownersedge-ratings-prod.us-east-1.elasticbeanstalk.com/',
     formSetUrl:'https://7xipl8fsz2.execute-api.us-east-1.amazonaws.com/dev/ownersedge/',
     formSetAPIKey:'TvNEPSgZQC9ZQivUv5JMZ3gi2iHFZtOA3imfivUL',
+
+    awsCognito:{
+        identityPoolId: 'us-east-1:ba19f09e-c0fe-4835-bce6-cf17ff45f4ef',
+        userPoolId: 'us-east-1_xHMTRwuln',
+        region: 'us-east-1',
+        clientId: '3n1q5qr2eldkqdnm98btc9dd9e'
+    },
+
     underwriters:[{
-        "name":"Jessica Buelow",
-        "position":"Supervisor",
-        "location":"New York",
-        "phone":"212-607-8829"
+        name: 'Jessica Buelow',
+        position: 'Supervisor',
+        location: 'New York',
+        phone: '212-607-8829'
     }],
 
     analytics: {
@@ -35,16 +43,15 @@ var config = {
     },
 
     pdfOptions: {
-
-        "height": "11in", // allowed units: mm, cm, in, px
-        "width": "8.5in", // allowed units: mm, cm, in, px
-        "format": "Letter", // allowed units: A3, A4, A5, Legal, Letter, Tabloid
-        "orientation": "portrait", // portrait or landscape
-        "border": {
-            "top": "12mm", // default is 0, units: mm, cm, in, px
-            "right": "6mm",
-            "bottom": "12mm",
-            "left": "6mm"
+        height: '11in', // allowed units: mm, cm, in, px
+        width: '8.5in', // allowed units: mm, cm, in, px
+        format: 'Letter', // allowed units: A3, A4, A5, Legal, Letter, Tabloid
+        orientation: 'portrait', // portrait or landscape
+        border: {
+            top: '12mm', // default is 0, units: mm, cm, in, px
+            right: '6mm',
+            bottom: '12mm',
+            left: '6mm'
         }
     },
 
@@ -75,19 +82,5 @@ if (config.env === 'development') {
         origin: 'http://localhost:' + config.webpackserver.port
     };
 }
-
-// else if (config.env === 'devdeploy') {
-//     config.server.url = 'http://dev.ownersedge.us';
-//     config.apiserver.url = 'http://dev.ownersedge.us';
-//     config.apiserver.port = null;
-// } else if (config.env === 'production') {
-//     config.server.url = 'http://ownersedge.us';
-//     config.apiserver.url = 'http://ownersedge.us';
-//     config.apiserver.port = null;
-// } else if (config.env === 'qaDeploy') {
-//     config.server.url = 'http://beta.ownersedge.us';
-//     config.apiserver.url = 'http://beta.ownersedge.us';
-//     config.apiserver.port = null;
-// }
 
 module.exports = config;
