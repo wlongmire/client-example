4b6c6de39cf27d8bd0fa4522667eaa7decb5fcfc
Andriy Kulak <andriy.kulak@NYC-C02SV4HSH03Q.local>
2017-07-14 15:40:12 -0400

    added test for save submissions

--- src/app/actions/__tests__/submissionViewAction.test.js
+++ src/app/actions/__tests__/submissionViewAction.test.js
@@ -4,13 +4,35 @@ import thunk from 'redux-thunk'
 import configureMockStore from 'redux-mock-store'
 import nock from 'nock'
 import * as actions from '../submissionActions'
-import { editSubmission } from '../submissionActions'
+import { editSubmission, saveSubmission } from '../submissionActions'
 import LocalStorageMock from '../../__mocks__/localStorageMock'
 import { SUBMISSION_STATUS, CHANGE_SUBMISSION_STATUS, CHANGE_SUBMISSION } from 'app/constants/submission'
 
+// global.checkTokenExpiration = () => { return Promise.resolve({ success: true }) }
+
+// MOCKING HTTP REQUESTS FOR FUTURE
+  // mocking the http request / response
+  // nock('http://localhost:8888')
+  //   .get('/api/getSubmission/randomId')
+  //   .reply(200, { submission: { _id: 'test123' } })
+  // var invokeUrl = `https://hotgyzwdq3.execute-api.us-east-1.amazonaws.com/${stageEnv}`;/api/getSubmission/{id}
+
+// MOCKING ****
+jest.mock('../userActions', () => 'testActions')
+const checkTokenExpiration = jest.fn().mockReturnValue(Promise.resolve({ success: true })) // eslint-disable-line
 global.apigClient = {
   apiGetSubmissionIdGet: () => {
-    console.log('using test PROMISE ================ 6666')
+    return Promise.resolve({
+      data: {
+        success: true,
+        submission: {
+          _id: 'test123',
+          primaryInsuredName: 'Test Name'
+        }
+      }
+    })
+  },
+  apiSavePost: () => {
     return Promise.resolve({
       data: {
         success: true,
@@ -23,58 +45,35 @@ global.apigClient = {
   }
 }
 
-// global.checkTokenExpiration = () => { return Promise.resolve({ success: true }) }
+describe('>>> Action - saveSubmission', () => {
+  it('SAVE SUBMISSION *****', () => {
+    // expect(
+    //   saveSubmission({ submission: 'bar' }, { id: 'test213234234' })
+    // ).toEqual({ submission: {}, submissionFormParams: {} })
 
-// MOCKING ****
-jest.mock('../userActions', () => 'testActions')
-const checkTokenExpiration = jest.fn().mockReturnValue(Promise.resolve({ success: true })) // eslint-disable-line
+    expect.assertions(1)
+    return saveSubmission(
+      { submission: {
+        primaryInsuredName: 'Test Name'
+      } },
+      { id: 'test123' })
+      .then(data => expect(data).toEqual(
+        { data: {
+          submission: {
+            _id: 'test123',
+            primaryInsuredName: 'Test Name'
+          },
+          success: true }
+        }))
+  })
+})
 
-// // describe('>>> Action - reset Form', () => {
-// //   it('successful call should dispatch someAction', () => {
-// //     // create mock thunk & store
-// //     const createMockStore = configureMockStore([thunk])
-// //     // expected results once action is dispatched
-// //     const expectedActions = [
-// //       { type: EDIT_SUBMISSION, payload: {} },
-// //       { payload: { args: ['/oiform'], method: 'push' },
-// //         type: '@@router/CALL_HISTORY_METHOD',
-// //       },
-// //     ]
-// //     const initialAppState = {}
-
-// //     const store = createMockStore(initialAppState)
-
-// //     // Act.
-// //     store.dispatch(actions.resetForm())
-
-// //     // Assert.
-// //     const dispatchedActions = store.getActions()
-// //     expect(dispatchedActions).toEqual(expectedActions)
-// //   })
-// // })
-
-export function fetchTodos() {
-  return (dispatch) => {
-    return dispatch({
-      type: 'test123123',
-      payload: {
-        submission: 'test123123',
-        submissionFormParams: 'test123123'
-      }
-    })
-  }
-}
 
 describe('>>> Action - editSubmission', () => {
   afterEach(() => {
     nock.cleanAll()
   })
 
-  // mocking the http request / response
-  // nock('http://localhost:8888')
-  //   .get('/api/getSubmission/randomId')
-  //   .reply(200, { submission: { _id: 'test123' } })
-  // var invokeUrl = `https://hotgyzwdq3.execute-api.us-east-1.amazonaws.com/${stageEnv}`;/api/getSubmission/{id}
   nock('https://hotgyzwdq3.execute-api.us-east-1.amazonaws.com/dev')
     .get('/api/getSubmission/randomId')
     .reply(200, { submission: { _id: 'test123' } })
