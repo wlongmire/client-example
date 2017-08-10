f64cde654237aa14ff040e718925a12bb7c2bbf3
Warren Longmire <warrenlongmire@gmail.com>
2017-08-10 16:40:45 -0400

    changing the port.

--- e2e/nightwatch.conf.BASIC.js
+++ e2e/nightwatch.conf.BASIC.js
@@ -12,7 +12,7 @@ module.exports = {
     'start_process': true, // tells nightwatch to start/stop the selenium process
     'server_path': './node_modules/nightwatch/bin/selenium.jar',
     'host': '127.0.0.1',
-    'port': 4444, // standard selenium port
+    'port': 4446, // standard selenium port
     'cli_args': { // chromedriver is downloaded by selenium-download (see readme)
       'webdriver.chrome.driver' : './node_modules/nightwatch/bin/chromedriver'
     }
