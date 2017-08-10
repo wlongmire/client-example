41c304007ed38b79cd7043a7901de41c91afe3f0
Warren Longmire <warrenlongmire@gmail.com>
2017-08-10 16:44:08 -0400

    start_process = false.

--- e2e/nightwatch.conf.BASIC.js
+++ e2e/nightwatch.conf.BASIC.js
@@ -9,10 +9,10 @@ module.exports = {
   ],
   'output_folder': './e2e/reports', // reports (test outcome) output by nightwatch
   'selenium': { // downloaded by selenium-download module (see readme)
-    'start_process': true, // tells nightwatch to start/stop the selenium process
+    'start_process': false, // tells nightwatch to start/stop the selenium process
     'server_path': './node_modules/nightwatch/bin/selenium.jar',
     'host': '127.0.0.1',
-    'port': 4446, // standard selenium port
+    'port': 4444, // standard selenium port
     'cli_args': { // chromedriver is downloaded by selenium-download (see readme)
       'webdriver.chrome.driver' : './node_modules/nightwatch/bin/chromedriver'
     }
