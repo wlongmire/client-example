09c5efc23e510df479bd3bcb001bb1c55eb2011c
Andriy Kulak <andriy.kulak@NYC-C02SV4HSH03Q.T-mobile.com>
2017-08-15 09:46:23 -0400

    another 100

--- e2e/utils/login.js
+++ e2e/utils/login.js
@@ -1,7 +1,7 @@
 function login(browser, username, password) {
   return browser
-      .url('http://ownersedge-dev.s3-website-us-east-1.amazonaws.com/')
-      // .url('http://localhost:7777/')
+      // .url('http://ownersedge-dev.s3-website-us-east-1.amazonaws.com/')
+      .url('http://localhost:7777/')
       .waitForElementVisible('body')
       .setValue('#username', username)
       .setValue('#password', [password, browser.Keys.ENTER])
@@ -9,4 +9,4 @@ function login(browser, username, password) {
       .waitForElementVisible('/html/body/div[1]/div/nav/div/div[2]/ul/li[2]/a', null, null, null, 'Login Successfull!')
 }
 
-module.exports = login;
+module.exports = login
