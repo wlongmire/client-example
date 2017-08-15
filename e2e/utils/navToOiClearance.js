9003649f3c7942b90c85d24ed2fa8c9a5d89efb3
Andriy Kulak <andriy.kulak@NYC-C02SV4HSH03Q.T-mobile.com>
2017-08-15 10:12:29 -0400

    added another thing to test

--- e2e/utils/navToOiClearance.js
+++ e2e/utils/navToOiClearance.js
@@ -1,7 +1,9 @@
 // navigate to ocp clearance form
 function navToOiClearance(browser) {
   return browser
+      .waitForElementVisible('/html/body/div[1]/div/nav/div/div[2]/ul/li[2]/a', null, null, null, 'Got To OI - Part 1')
       .click('/html/body/div[1]/div/nav/div/div[2]/ul/li[2]/a')
+      .waitForElementVisible('/html/body/div[1]/div/div/div/div[1]', null, null, null, 'Got To OI - Part 2')
       .click('/html/body/div[1]/div/div/div/div[1]')
 }
 
