353e2f205fbc4e98288b7e0e4fe20760ef3da45e
Warren Longmire <warrenlongmire@gmail.com>
2017-08-16 10:24:43 -0400

    adding console for sub save.

--- src/app/routes/FormResults/Loading/index.jsx
+++ src/app/routes/FormResults/Loading/index.jsx
@@ -55,6 +55,8 @@ class Loading extends Component {
           this.props.logout()
         }
 
+        console.log(respSave)
+
         if (respSave.data && respSave.data.success === true) {
           const { submissionId } = respSave.data
           const mainRating = ratings[submission.type]
