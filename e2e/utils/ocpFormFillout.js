c39e992fdecc43189afb9d012b36fac00be37a26
Andriy Kulak <andriy.kulak@NYC-C02SV4HSH03Q.local>
2017-09-06 12:18:42 -0400

    updated e2e tests

--- e2e/utils/ocpFormFillout.js
+++ e2e/utils/ocpFormFillout.js
@@ -1,6 +1,9 @@
 function ocpFormFillout(browser) {
   return browser
       .useCss()
+      .waitForElementVisible('.sub-header-firstRow', 30000, null, null, 'You got to the the form screen!')
+      .assert.containsText('.sub-header-firstRow', 'CLEARANCE PASSED', 'Passed Clearance')
+      .pause(1000)
       .setValue('#totalCost', 5000000)
       .setValue('#anticipatedStartDate', '10/17/2017')
       .setValue('#anticipatedFinishDate', '10/17/2019')
@@ -9,10 +12,10 @@ function ocpFormFillout(browser) {
       .setValue('#phone', 7182072578)
       .setValue('#limitsRequested', '1/2')
       .useXpath()
-      .click('/html/body/div[1]/div/div/form/div/div[9]/div[2]/div/div/div[1]/div[1]/label/input')  // vertical expansion - NO
-      .click('/html/body/div[1]/div/div/form/div/div[16]/div[2]/div/div/div[1]/div[1]/label/input') // project requirements - NO
-      .click('/html/body/div[1]/div/div/form/div/div[18]/div[2]/div/div/div[1]/div[1]/label/input') // exterior Work over 4 stories - NO
-      .click('/html/body/div[1]/div/div/form/div/div[19]/div[2]/div/div[1]/div[1]/div[1]/label/input') // servicingServeralLocations - NO
+      .click('/html/body/div[1]/div/div/form/div/div[9]/div[2]/div/div/div[1]/div[2]/label/input') // vertical expansion - NO
+      .click('/html/body/div[1]/div/div/form/div/div[16]/div[2]/div/div/div[1]/div[2]/label/input') // project requirements - NO
+      .click('/html/body/div[1]/div/div/form/div/div[18]/div[2]/div/div/div[1]/div[2]/label/input') // exterior Work over 4 stories - NO
+      .click('/html/body/div[1]/div/div/form/div/div[19]/div[2]/div/div[1]/div[1]/div[2]/label/input') // servicingServeralLocations - NO
 }
 
 module.exports = ocpFormFillout
