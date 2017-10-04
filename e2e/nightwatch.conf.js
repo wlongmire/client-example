111cef735f3f8441b35f6d525b05d40bc7cdad36
Warren Longmire <warrenlongmire@gmail.com>
2017-10-04 13:29:37 -0400

    cleanup of nightwatch code.

--- e2e/nightwatch.conf.js
+++ e2e/nightwatch.conf.js
@@ -1,41 +1,41 @@
 // require('env2')('.env'); // optionally store your Evironment Variables in .env
-const SCREENSHOT_PATH = "./e2e/screenshots/";
-const BINPATH = './../node_modules/nightwatch/bin/';
+const SCREENSHOT_PATH = './e2e/screenshots/'
+const BINPATH = './../node_modules/nightwatch/bin/'
 
 // we use a nightwatch.conf.js file so we can include comments and helper functions
 module.exports = {
-  "src_folders": [
-    "./e2e/tests"// Where you are storing your Nightwatch e2e tests
+  src_folders: [
+    './e2e/tests'// Where you are storing your Nightwatch e2e tests
   ],
-  "output_folder": "./e2e/reports", // reports (test outcome) output by nightwatch
-  "selenium": { // downloaded by selenium-download module (see readme)
-    "start_process": true, // tells nightwatch to start/stop the selenium process
-    "server_path": "./../node_modules/nightwatch/bin/selenium.jar",
-    "host": "127.0.0.1",
-    "port": 4444, // standard selenium port
-    "cli_args": { // chromedriver is downloaded by selenium-download (see readme)
-      "webdriver.chrome.driver" : "./../node_modules/nightwatch/bin/chromedriver"
+  output_folder: './e2e/reports', // reports (test outcome) output by nightwatch
+  selenium: { // downloaded by selenium-download module (see readme)
+    start_process: true, // tells nightwatch to start/stop the selenium process
+    server_path: './../node_modules/nightwatch/bin/selenium.jar',
+    host: '127.0.0.1',
+    port: 4444, // standard selenium port
+    cli_args: { // chromedriver is downloaded by selenium-download (see readme)
+      webdriver.chrome.driver : './../node_modules/nightwatch/bin/chromedriver'
     }
   },
-  "test_settings": {
-    "default": {
-      "screenshots": {
-        "enabled": false, // if you want to keep screenshots
-        "path": './e2e/screenshots' // save screenshots here
+  test_settings: {
+    default: {
+      screenshots: {
+        enabled: false, // if you want to keep screenshots
+        path: './e2e/screenshots' // save screenshots here
       },
-      "globals": {
-        "waitForConditionTimeout": 5000 // sometimes internet is slow so wait.
+      globals: {
+        'waitForConditionTimeout': 5000 // sometimes internet is slow so wait.
       },
-      "desiredCapabilities": { // use Chrome as the default browser for tests
-        "browserName": "chrome"
+      desiredCapabilities: { // use Chrome as the default browser for tests
+        browserName: 'chrome'
       }
     },
-    "chrome": {
-      "desiredCapabilities": {
-        "browserName": "chrome",
-        "javascriptEnabled": true, // turn off to test progressive enhancement,
-        "chromeOptions": {
-          "args" : ["--no-sandbox", "headless", "disable-gpu"]
+    chrome: {
+      desiredCapabilities: {
+        browserName: 'chrome',
+        javascriptEnabled: true, // turn off to test progressive enhancement,
+        chromeOptions: {
+          args : ['--no-sandbox', 'headless', 'disable-gpu']
         }
       }
     }
@@ -61,7 +61,7 @@ function padLeft (count) { // theregister.co.uk/2016/03/23/npm_left_pad_chaos/
   return count < 10 ? '0' + count : count.toString();
 }
 
-var FILECOUNT = 0; // "global" screenshot file count
+var FILECOUNT = 0; // 'global' screenshot file count
 /**
  * The default is to save screenshots to the root of your project even though
  * there is a screenshots path in the config object above! ... so we need a
