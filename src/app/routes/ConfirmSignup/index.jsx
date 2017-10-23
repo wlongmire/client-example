29cff93d58a4a5eafb7e6d6cd7361ced0f1bee87
Andriy Kulak <andriy.kulak@NYC-C02SV4HSH03Q.local>
2017-10-23 14:51:30 -0400

    added another route

---
+++ src/app/routes/ConfirmSignup/index.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import { Button } from 'react-bootstrap'
+
+class ConfirmSignup extends Component {
+  render() {
+    console.log('this.props.location.query.key', this.props.location.query.key)
+    const test123 = () => {
+      return apigClient.adminUsersPost({}, {
+        "email": "andku..l.a.k@gmail.com",
+        "broker_id": "test-7fd-b3ff-4fd3-9fc2-e752b9f5b002",
+        "role": "admin",
+        "status": "pending",
+        lastOnline: new Date().toISOString()
+      }).then((response) => {
+        console.log("RESPONSE =====>", response)
+      })
+    }
+    return (
+      <div>
+        <Button onClick={() => { return test123() }}>Test123</Button>
+        <div>TESTING CONFIRM SIGNUP COMPONENT {this.props.location.query.key}</div>
+      </div>
+    )
+  }
+}
+
+export default ConfirmSignup
